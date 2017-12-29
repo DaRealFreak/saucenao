e1c3242cc91255a277a7fd6d00ec1d35ca3b0f88
Steffen Keuper <steffen.keuper@web.de>
2017-12-29 15:37:13 +0100

    [TASK] implement excludes, categorization, minimum similarity, add option to combine html and json results

--- saucenao.py
+++ saucenao.py
@@ -10,6 +10,7 @@ import time
 from HTMLParser import HTMLParser
 # noinspection PyProtectedMember
 from mimetypes import MimeTypes
+from shutil import move
 
 import requests
 from bs4 import BeautifulSoup as Soup
@@ -64,6 +65,24 @@ class FileHandler:
             text = text.replace(char, replacement[chars.index(char)])
         return text
 
+    @staticmethod
+    def move_to_category(filename, category):
+        """
+        move file to the sub_category folder
+
+        :param filename:
+        :param category:
+        :return:
+        """
+        logger.info(u"moving {0:s} to category: {1:s}".format(filename, category))
+        folder = FileHandler.ensure_unicode(re.sub(r'["/?*:<>|]', r'', category))
+        folder = os.path.join(args.dir, 'testing', folder)
+        folder = os.path.abspath(FileHandler.unicode_translate(folder, u"\n\t\r", u"   "))
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        move(os.path.join(args.dir, filename), os.path.join(folder, filename))
+
 
 class SauceNao(object):
     """"
@@ -77,8 +96,12 @@ class SauceNao(object):
     LIMIT_30_SECONDS = 20
 
     # 0=html, 2=json but json is omitting important data but includes more data about authors
-    # ToDo: add option to merge both results
-    OUTPUT_TYPE = 0
+    # taken from the API documentation(requires login): https://saucenao.com/user.php?page=search-api
+    API_HTML_TYPE = 0
+    API_JSON_TYPE = 2
+
+    CONTENT_CATEGORY_KEY = 'Material'
+    CONTENT_CHARACTERS_KEY = 'Characters'
 
     directory = None
     mime = None
@@ -87,29 +110,115 @@ class SauceNao(object):
         """
         initializing function
 
+        :type directory: str
         :param directory:
         """
         self.directory = directory
         self.mime = MimeTypes()
 
         files = FileHandler.get_files(directory)
+
+        if args.exclude_categories:
+            excludes = [l.lower() for l in args.exclude_categories.split(",")]
+        else:
+            excludes = []
+
         for file_name in files:
             start_time = time.time()
-            result_html = self.check_image(file_name)
-            sorted_results = self.parse_results_json(result_html, file_name)
+            result = self.check_image(file_name, self.API_HTML_TYPE)
+            sorted_results = self.parse_results_json(result, file_name)
 
-            from pprint import pprint
-            pprint(sorted_results)
+            if args.combine_api_types:
+                additional_result = self.check_image(file_name, self.API_JSON_TYPE)
+                additional_sorted_results = self.parse_results_json(additional_result, file_name)
+                sorted_results = self.merge_results(sorted_results, additional_sorted_results)
+
+            filtered_results = self.filter_results(sorted_results)
+
+            if args.move_to_categories:
+                category = self.get_content_value(filtered_results, self.CONTENT_CATEGORY_KEY)
+                if not category:
+                    logger.info(u"no category found for file: {0:s}".format(file_name))
+                    continue
+
+                # sub categories we don't want to move like original etc
+                if category.lower() in excludes:
+                    logger.info(u"skipping excluded category: {0:s} ({1:s})".format(category, file_name))
+                    continue
+
+                FileHandler.move_to_category(file_name, category)
+            else:
+                # ToDo: what exactly is the default case I want here?
+                # possibly printing the list
+                pass
 
             duration = time.time() - start_time
             if duration < (30 / self.LIMIT_30_SECONDS):
                 logger.debug("sleeping '{:.2f}' seconds".format((30 / self.LIMIT_30_SECONDS) - duration))
                 time.sleep((30 / self.LIMIT_30_SECONDS) - duration)
 
-    def check_image(self, file_name):
+    @staticmethod
+    def get_content_value(results, key):
+        """
+        return the first match of Material in content
+        multiple sites have a categorisation which saucenao utilizes to provide it in the content section
+
+        :type results: list
+        :type key: str
+        :param results:
+        :param key:
+        :return:
+        """
+        for result in results:
+            if 'content' in result['data'].keys():
+                for content in result['data']['content']:
+                    if re.match('{0:s}: .*'.format(key), content):
+                        return ''.join(re.split(r'{0:s}: '.format(key), content)[1:])
+        return ''
+
+    @staticmethod
+    def merge_two_dicts(x, y):
+        """
+        take x dictionary and insert/overwrite y dictionary values
+
+        :type x: dict
+        :type y: dict
+        :param x:
+        :param y:
+        :return:
+        """
+        z = x.copy()  # start with x's keys and values
+        z.update(y)  # modifies z with y's keys and values & returns None
+        return z
+
+    def merge_results(self, result, additional_result):
+        """
+        merge two result arrays
+
+        :type result: list
+        :type additional_result: list
+        :param result:
+        :param additional_result:
+        :return:
+        """
+        if len(result) <= len(additional_result):
+            length = len(result)
+        else:
+            length = len(additional_result)
+
+        for i in xrange(length):
+            for key in result[i].keys():
+                result[i][key] = self.merge_two_dicts(result[i][key], additional_result[i][key])
+
+        return result
+
+    def check_image(self, file_name, output_type):
         """
         check the possible sources for the given file
 
+        :type output_type: int
+        :type file_name: str
+        :param output_type:
         :param file_name:
         :return:
         """
@@ -133,7 +242,7 @@ class SauceNao(object):
             'frame': 1,
             'hide': 0,
             # parameters taken from API documentation: https://saucenao.com/user.php?page=search-api
-            'output_type': self.OUTPUT_TYPE,
+            'output_type': output_type,
             'db': args.databases,
         }
 
@@ -142,7 +251,7 @@ class SauceNao(object):
 
         link = requests.post(url=self.SEARCH_POST_URL, files=files, params=params, headers=headers)
 
-        if self.OUTPUT_TYPE == 0:
+        if output_type == self.API_HTML_TYPE:
             return self.parse_results_html_to_json(link.text)
 
         return link.text
@@ -152,6 +261,7 @@ class SauceNao(object):
         """
         parse the results and sort them descending by similarity
 
+        :type html: str
         :param html:
         :return:
         """
@@ -164,10 +274,10 @@ class SauceNao(object):
             similarity = res.find_next('div', attrs={"class": "resultsimilarityinfo"}).text.replace('%', '')
             alternate_links = [a_tag['href'] for a_tag in
                                res.find_next('div', attrs={"class": "resultmiscinfo"}).find_all('a', href=True)]
-            content_column_tag = res.find_next('div', attrs={"class": "resultcontentcolumn"})
-            for br in content_column_tag.find_all("br"):
-                br.replace_with("\n")
-            content_column = content_column_tag.text
+            content_column = []
+            content_column_tags = res.find_all('div', attrs={"class": "resultcontentcolumn"})
+            for content_column_tag in content_column_tags:
+                content_column.append(content_column_tag.text)
 
             result = {
                 'header': {
@@ -188,6 +298,8 @@ class SauceNao(object):
         """
         parse the results and sort them descending by similarity
 
+        :type text: str
+        :type file_name: str
         :param text:
         :param file_name:
         :return:
@@ -200,19 +312,39 @@ class SauceNao(object):
         results = [res for res in result['results']]
         return sorted(results, key=lambda k: float(k['header']['similarity']), reverse=True)
 
+    @staticmethod
+    def filter_results(sorted_results):
+        """
+        return results with a similarity bigger or the same as the defined similarity from the arguments (default 65%)
+
+        :type sorted_results: list
+        :param sorted_results:
+        :return:
+        """
+        filtered_results = []
+        for res in sorted_results:
+            if float(res['header']['similarity']) >= float(args.minimum_similarity):
+                filtered_results.append(res)
+            else:
+                # we can break here since the results are sorted by similarity anyways
+                break
+        return filtered_results
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--dir', help='directory to sort', required=True)
     parser.add_argument('-db', '--databases', default=999, type=int, help='which databases should be searched')
-    parser.add_argument('-m', '--minimum-match', default=65, type=int, help='minimum similarity percentage')
+    parser.add_argument('-min', '--minimum_similarity', default=65, type=float, help='minimum similarity percentage')
+    parser.add_argument('-c', '--combine-api-types', action='store_true', help='combine html and json api response to '
+                                                                               'retrieve more information')
     parser.add_argument('-k', '--api-key', help='API key of your account on saucenao')
     parser.add_argument('-x', '--exclude-categories', type=str, help='exclude specific categories from moving')
-    parser.add_argument('-mv', '--move-to-categories', default=False, type=bool, help='move images to categories')
+    parser.add_argument('-mv', '--move-to-categories', action='store_true', help='move images to categories')
     args = parser.parse_args()
 
     logging.basicConfig()
     logger = logging.getLogger("logger")
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
 
     SauceNao(args.dir)
