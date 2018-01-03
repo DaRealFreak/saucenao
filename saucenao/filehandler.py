ac0820ed983422917b423ab173bdace2c35a6c0d
Steffen Keuper <steffen.keuper@web.de>
2018-01-03 10:26:57 +0100

    [TASK] remove ensure_unicode function since it's default in Python 3

--- saucenao/filehandler.py
+++ saucenao/filehandler.py
@@ -26,18 +26,6 @@ class FileHandler:
                 yield f
 
     @staticmethod
-    def ensure_unicode(text):
-        """
-        ensure unicode encoding
-
-        :param text:
-        :return:
-        """
-        if re.match(r'.*&#[\d]{2,6};.*', text):
-            return FileHandler.ensure_unicode(html.unescape(text))
-        return str(text)
-
-    @staticmethod
     def unicode_translate(text, chars="", replacement=""):
         """
         replacement for the string.maketrans function
@@ -61,7 +49,7 @@ class FileHandler:
         :param category:
         :return:
         """
-        folder = FileHandler.ensure_unicode(re.sub(r'["/?*:<>|]', r'', category))
+        folder = re.sub(r'["/?*:<>|]', r'', category)
         folder = os.path.join(base_directory, folder)
         folder = os.path.abspath(FileHandler.unicode_translate(folder, "\n\t\r", "   "))
         if not os.path.exists(folder):
