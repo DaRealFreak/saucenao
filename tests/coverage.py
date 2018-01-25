6c08ce5b68bd3f91a78c730ef3b0917edf476e7a
Steffen Keuper <steffen.keuper@web.de>
2018-01-25 18:31:35 +0100

    [TASK] remove coverage.py since it is covered by nosetests now

--- tests/coverage.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/python
-# coding: utf-8
-import unittest
-
-from tests.files import __all__
-
-for test_case in __all__:
-    suite = unittest.TestLoader().loadTestsFromTestCase(test_case)
-    unittest.TextTestRunner(verbosity=2).run(suite)
