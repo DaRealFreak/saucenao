64e2e817815024137163d5de19151c1ede2bfc78
Steffen Keuper <steffen.keuper@web.de>
2020-06-03 13:07:18 +0200

    [TASK] add additional test for io.BytesIO objects

--- tests/test_saucenao.py
+++ tests/test_saucenao.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import io
 import os
 import shutil
 import unittest
@@ -42,15 +42,29 @@ class TestSauceNao(unittest.TestCase):
         im.save(file_path, "JPEG")
         return file_path
 
-    def test_run_application(self):
+    def test_check_file_object(self):
         """Test the run_application function
 
         :return:
         """
+        # check returned object from file open
         with open(self.generate_small_jpg(), "rb") as test_file:
             results = self.saucenao.check_file_object(test_file)
             self.assertIsInstance(results, list)
             print(results)
+
+        # check io.BytesIO object
+        results = self.saucenao.check_file_object(
+            io.BytesIO(
+                b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A\x00\x00\x00\x0D\x49\x48\x44\x52\x00\x00\x01\x00\x00\x00\x01\x00"
+                b"\x01\x03\x00\x00\x00\x66\xBC\x3A\x25\x00\x00\x00\x03\x50\x4C\x54\x45\xB5\xD0\xD0\x63\x04\x16\xEA"
+                b"\x00\x00\x00\x1F\x49\x44\x41\x54\x68\x81\xED\xC1\x01\x0D\x00\x00\x00\xC2\xA0\xF7\x4F\x6D\x0E\x37"
+                b"\xA0\x00\x00\x00\x00\x00\x00\x00\x00\xBE\x0D\x21\x00\x00\x01\x9A\x60\xE1\xD5\x00\x00\x00\x00\x49"
+                b"\x45\x4E\x44\xAE\x42\x60\x82"
+            )
+        )
+        self.assertIsInstance(results, list)
+        print(results)
 
 
 if __name__ == '__main__':
